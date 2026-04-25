26929598e0667b2adc7756353035df7058becf25
freelanceradnan <reactorbro722@gmail.com>
2026-04-26 02:14:07 +0600

    home pages added with hero sec

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import { useLocation } from 'react-router'
+import { Route, Routes, useLocation } from 'react-router'
+import Home from './Pages/Home/Home';
 
 
 
@@ -11,7 +12,14 @@ const isAdminPath=useLocation().pathname.includes("admin-dashboard")
 
   return (
   <>
+  {/* navbar */}
    {!isAdminPath && <Navbar/>}
+   {/* all-routes */}
+   <div className="min-h-[70vh]">
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+   </Routes>
+   </div>
   </>
   )
 }
